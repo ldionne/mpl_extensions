16256eb53bfe79a86405ea925d1716e8bfbd4d60
Louis Dionne <ldionne.2@gmail.com>
2013-08-22 13:18:28 -0400

    Refactor the pattern matching.

--- include/boost/mpl/matches.hpp
+++ include/boost/mpl/matches.hpp
@@ -6,114 +6,84 @@
 #ifndef BOOST_MPL_MATCHES_HPP
 #define BOOST_MPL_MATCHES_HPP
 
-#include <boost/mpl/all_of.hpp>
-#include <boost/mpl/and.hpp>
 #include <boost/mpl/any_of.hpp>
 #include <boost/mpl/bool.hpp>
+#include <boost/mpl/equal.hpp>
 #include <boost/mpl/not.hpp>
 #include <boost/mpl/placeholders.hpp>
-#include <boost/mpl/unpack_args.hpp>
 #include <boost/mpl/vector.hpp>
-#include <boost/mpl/zip_view.hpp>
 #include <boost/type_traits/is_same.hpp>
 
 
 namespace boost { namespace mpl {
-namespace matches_detail {
-//! By default, a type matches a pattern if they are the same.
-//! Note: specializing matches<T, T> makes it ambiguous with the Node<...>
-//!       specialization when Children1 is the same as Children2.
+namespace match {
+    //! Matches anything.
+    struct any;
+
+    // Matches iff any of the patterns match.
+    template <typename ...Patterns>
+    struct any_of;
+
+    //! Matches iff the pattern does not match.
+    template <typename Pattern>
+    struct not_;
+} // end namespace match
+
+/*!
+ * Metafunction returning whether a type matches a given pattern.
+ *
+ * By default, a type matches a pattern if they are the same. Two template
+ * types match if their template parameters match side by side.
+ *
+ *
+ * @internal
+ * We use `is_same` because specializing `matches<T, T>` makes it an ambiguous
+ * specialization with the `Node<...>` specialization when `Children1` is the
+ * same as `Children2`.
+ */
 template <typename T, typename Pattern>
 struct matches
     : is_same<T, Pattern>
 { };
 
-//! Two template classes match if their template parameters match side by side.
 template <template <typename ...> class Node,
           typename ...Children1,
           typename ...Children2>
 struct matches<Node<Children1...>, Node<Children2...>>
-    : and_<
-        bool_<sizeof...(Children1) == sizeof...(Children2)>,
-        all_of<
-            zip_view<
-                typename vector<
-                    typename vector<Children1...>::type,
-                    typename vector<Children2...>::type
-                >::type
-            >,
-            unpack_args<matches<_1, _2>>
-        >
+    : equal<
+        typename vector<Children1...>::type,
+        typename vector<Children2...>::type,
+        matches<_1, _2>
     >
 { };
 
-//! Matches anything.
-struct match_any;
 template <typename T>
-struct matches<T, match_any> : true_ { };
+struct matches<T, match::any>
+    : true_
+{ };
 
-//! Matches if any of the patterns match.
-template <typename ...Patterns> struct match_any_of;
 template <typename T, typename ...Patterns>
-struct matches<T, match_any_of<Patterns...>>
+struct matches<T, match::any_of<Patterns...>>
     : any_of<
         typename vector<Patterns...>::type,
         matches<T, _1>
     >
 { };
 
-//! Matches if all of the patterns match.
-template <typename ...Patterns> struct match_all_of;
-template <typename T, typename ...Patterns>
-struct matches<T, match_all_of<Patterns...>>
-    : all_of<
-        typename vector<Patterns...>::type,
-        matches<T, _1>
-    >
-{ };
-
-//! Matches iff the pattern does not match.
-template <typename Pattern> struct dont_match;
 template <typename T, typename Pattern>
-struct matches<T, dont_match<Pattern>>
+struct matches<T, match::not_<Pattern>>
     : not_<matches<T, Pattern>>
 { };
 
-template <typename ...Patterns>
-struct matches_any_of {
-    template <typename T>
-    struct apply
-        : any_of<
-            typename vector<Patterns...>::type,
-            matches<T, _1>
-        >
+namespace matches_detail {
+    template <typename FuncDecl>
+    struct assert_matches_impl;
+
+    template <typename Ret, typename T, typename Pattern>
+    struct assert_matches_impl<Ret (T, Pattern)>
+        : matches<T, Pattern>
     { };
-};
-
-//! Matches if all of the children of the node matches any of the patterns.
-template <template <typename ...> class Node,
-          typename ...Children,
-          typename ...Patterns>
-struct matches<Node<Children...>, Node<match_all_of<Patterns...>>>
-    : all_of<
-        typename vector<Children...>::type,
-        matches_detail::matches_any_of<Patterns...>
-    >
-{ };
-
-template <typename FuncDecl> struct assert_matches_impl;
-
-template <typename Ret, typename T, typename Pattern>
-struct assert_matches_impl<Ret (T, Pattern)>
-    : matches<T, Pattern>
-{ };
 } // end namespace matches_detail
-
-using matches_detail::dont_match;
-using matches_detail::match_all_of;
-using matches_detail::match_any;
-using matches_detail::match_any_of;
-using matches_detail::matches;
 
 #define BOOST_MPL_ASSERT_MATCHES(FuncParams)                                \
     static_assert(                                                          \
@@ -127,7 +97,7 @@ using matches_detail::matches;
 #define BOOST_MPL_ASSERT_MATCHES_NOT(FuncParams)                            \
     static_assert(                                                          \
       ! ::boost::mpl::matches_detail::assert_matches_impl<                  \
-        void FuncParams                                                     \
+            void FuncParams                                                 \
         >::value,                                                           \
         #FuncParams                                                         \
     )                                                                       \
--- test/matches.cpp
+++ test/matches.cpp
@@ -9,34 +9,39 @@
 using namespace boost::mpl;
 
 struct Leaf;
-template <typename...> struct Node;
+template <typename ...> struct Node;
 
+// Pattern matching with non-template types
 BOOST_MPL_ASSERT_MATCHES((Leaf, Leaf));
 BOOST_MPL_ASSERT_MATCHES_NOT((Leaf, struct some_other_type));
-BOOST_MPL_ASSERT_MATCHES((Leaf, match_any));
 
+// Pattern matching with template types
 BOOST_MPL_ASSERT_MATCHES((Node<Leaf>, Node<Leaf>));
-BOOST_MPL_ASSERT_MATCHES((Node<Leaf>, Node<match_any>));
-BOOST_MPL_ASSERT_MATCHES_NOT((Node<>, Node<match_any>));
-
+BOOST_MPL_ASSERT_MATCHES_NOT((Node<Leaf>, Node<struct other>));
 BOOST_MPL_ASSERT_MATCHES((
-    Node<Node<Leaf>, Node<>, Node<Leaf>>,
-    Node<Node<match_any>, Node<>, Node<Leaf>>
+    Node<Node<Leaf>, struct X>,
+    Node<Node<Leaf>, struct X>
 ));
-
+BOOST_MPL_ASSERT_MATCHES_NOT((
+    Node<Node<Leaf>, struct X>,
+    Node<Node<Leaf>, struct Y>
+));
 BOOST_MPL_ASSERT_MATCHES_NOT((
     Node<Node<>, Node<>>,
     Node<Node<Leaf>, Node<>>
 ));
 
-BOOST_MPL_ASSERT_MATCHES_NOT((
-    struct some_type,
-    match_all_of<struct another_type, struct some_type>
-));
-
+// Pattern matching with the `match::any` keyword.
+BOOST_MPL_ASSERT_MATCHES((Leaf, match::any));
+BOOST_MPL_ASSERT_MATCHES((Node<>, match::any));
+BOOST_MPL_ASSERT_MATCHES((Node<Leaf>, match::any));
+// note: the `match::any` on the left should not be considered as a keyword.
+BOOST_MPL_ASSERT_MATCHES((Node<match::any>, match::any));
+BOOST_MPL_ASSERT_MATCHES((Node<Leaf>, Node<match::any>));
+BOOST_MPL_ASSERT_MATCHES_NOT((Node<>, Node<match::any>));
 BOOST_MPL_ASSERT_MATCHES((
-    Node<struct leaf2, struct leaf0, struct leaf1>,
-    Node<match_all_of<struct leaf0, struct leaf1, struct leaf2>>
+    Node<Node<Leaf>, Node<>, Node<Leaf>>,
+    Node<Node<match::any>, Node<>, Node<Leaf>>
 ));
 
 
